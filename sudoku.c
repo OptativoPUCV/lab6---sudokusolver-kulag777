7a1f9b3c5498cd11bc9e5e83fb9facde2e1ef07c
Your Name <you@example.com>
2024-05-16 15:58:17 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -1,69 +1,61 @@
+#include "list.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
 
+typedef struct {
+  int sudo[9][9];
+} Node;
 
-typedef struct{
-   int sudo[9][9];
-}Node;
-
-Node* createNode(){
-  Node* n=(Node*) malloc(sizeof(Node));
+Node *createNode() {
+  Node *n = (Node *)malloc(sizeof(Node));
   return n;
 }
 
-Node* copy(Node* n){
-    Node* new=(Node*) malloc(sizeof(Node));
-    *new = *n;
-    return new;
+Node *copy(Node *n) {
+  Node *new = (Node *)malloc(sizeof(Node));
+  *new = *n;
+  return new;
 }
 
-Node* read_file (char* file_name){
-  Node* n = createNode();
-  FILE* file = fopen (file_name, "r");
-  int i,j;
-  for(i=0;i<9;i++){
-       for(j=0;j<9;j++){
-          if(!fscanf (file, "%d", &n->sudo[i][j]))
-            printf("failed to read data!");
-       }
+Node *read_file(char *file_name) {
+  Node *n = createNode();
+  FILE *file = fopen(file_name, "r");
+  int i, j;
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++) {
+      if (!fscanf(file, "%d", &n->sudo[i][j]))
+        printf("failed to read data!");
+    }
   }
 
-  fclose (file);
+  fclose(file);
   return n;
 }
 
-void print_node(Node* n){
-    int i,j;
-    for(i=0;i<9;i++){
-       for(j=0;j<9;j++)
-          printf("%d ", n->sudo[i][j]);
-       printf("\n");
-    }
+void print_node(Node *n) {
+  int i, j;
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++)
+      printf("%d ", n->sudo[i][j]);
     printf("\n");
+  }
+  printf("\n");
 }
 
-int is_valid(Node* n){
-
-    return 1;
+int is_valid(Node *n) {
+   
+  return 1;
 }
 
-
-List* get_adj_nodes(Node* n){
-    List* list=createList();
-    return list;
+List *get_adj_nodes(Node *n) {
+   List *list = createList();
+   
+   return list;
 }
 
+int is_final(Node *n) { return 0; }
 
-int is_final(Node* n){
-    return 0;
-}
-
-Node* DFS(Node* initial, int* cont){
-  return NULL;
-}
-
-
+Node *DFS(Node *initial, int *cont) { return NULL; }
 
 /*
 int main( int argc, char *argv[] ){
