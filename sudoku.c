7b0bee140a9bcf1bb95ab2fcf9aa1f6215879f83
Your Name <you@example.com>
2024-05-16 16:09:57 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -42,27 +42,25 @@ void print_node(Node *n) {
   printf("\n");
 }
 
-int is_valid(Node *n) {
-   
-  return 1;
-}
+int is_valid(Node *n) { return 1; }
 
 List *get_adj_nodes(Node *n) {
-   List *list = createList();
-   int i, j;
-   for (i = 0; i < 9; i++)
-      {
-         for(j = 0; j < 9; j++)
-            {
-               if(n->sudo[i][j] == 0){
-                  Node *aux = copy(n);
-                  aux->sudo[i][j] = 1;
-                  pushBack(list, aux);
-               
-               }
-            }
+  List *list = createList();
+  int i, j;
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++) {
+      for (int k = 1; k <= 9; k++) {
+        if (n->sudo[i][j] == 0) {
+          Node *adj = copy(n);
+          adj->sudo[i][j] = k;
+          if (isvalid(adj))
+            ;
+          { pushBack(list, adj); }
+        }
       }
-   return list;
+    }
+  }
+  return list;
 }
 
 int is_final(Node *n) { return 0; }
