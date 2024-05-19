32ed78af54b8cc56c8a3b9bad76ded63c6ccabf9
Your Name <you@example.com>
2024-05-19 22:27:45 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -42,7 +42,29 @@ void print_node(Node *n) {
   printf("\n");
 }
 
-int is_valid(Node *n) { return 1; }
+int is_valid(Node *n) {
+  int rowCheck[9][10] = {0};
+  int colCheck[9][10] = {0};
+  int subgridCheck[3][3][10] = {0};
+  for (int i = 0; i < 9; ++i) {
+    for (int j = 0; j < 9; ++j) {
+      int num = n->sudo[i][j];
+      if (num == 0)
+        continue;
+      if (rowCheck[i][num] || colCheck[j][num])
+        return 0;
+      rowCheck[i][num] = 1;
+      colCheck[j][num] = 1;
+
+      int subgridRow = i / 3;
+      int subgridCol = j / 3;
+      if (subgridCheck[subgridRow][subgridCol][num])
+        return 0;
+      subgridCheck[subgridRow][subgridCol][num] = 1;
+    }
+  }
+  return 1;
+}
 
 List *get_adj_nodes(Node *n) {
   List *list = createList();
